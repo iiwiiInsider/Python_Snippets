9c908765cad4507c3ff7c9095a37711513b177b7
iiwiiInsider <kdbburns.za@gmail.com>
2026-06-23 14:17:08 +0200

    IDE Config files Integrity change and Python Scripts/Snippets added

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Hello and welcome to the band name generator!")
+city = input("To get your very own band name please enter your city of birth:\n")
+pet = input("Your pet's name:\n")
+print("Your band name could be: " + city + " " + pet)
